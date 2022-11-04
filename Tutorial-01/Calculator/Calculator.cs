62d35388f56b23b00d2dba35a3d29424113569f8
Natnicha <nat.rodtong@gmail.com>
2022-11-04 20:45:15 +0100

    feat: add tutorial-01 Task 2

---
+++ Tutorial-01/Calculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatorNS
+{
+    public class Calculator
+    {
+        public Calculator() {}
+
+        public int Multiplication(int val1, int val2)
+        {
+            return val1*val2;
+        }
+
+        public Decimal Division(int val1, int val2) { 
+            return (Decimal)val1/val2;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
