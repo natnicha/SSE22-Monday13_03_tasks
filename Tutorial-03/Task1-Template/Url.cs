779ad551373f19af2dc9b7f6db302fd634edde67
Natnicha <nat.rodtong@gmail.com>
2022-11-20 22:13:40 +0100

    feat: add Tutorial-3 task 1 & 3

---
+++ Tutorial-03/Task1-Template/Url.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using static System.Net.WebRequestMethods;
+
+namespace Task1
+{
+    /// <summary>
+    /// A class for generating and parsing HTTP-URIs.
+    /// </summary>
+    public class Url
+    {
+        const string VALID_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$-_.~";
+
+        public string Scheme = "";
+        public string Host = "";
+        public int Port = 80;
+        public string Path = "";
+        public string Query = "";
+        public string FragmentId = "";
+
+        /// <summary>
+        /// Constructor for parsing URLs.
+        /// </summary>
+        public Url(string urlStr)
+        {
+            string urlRegex = @"^(http|https|sftp|mailto|JDBC)(:\/{2})([\w*.-]*\w*)(:\d*)?(\/\w*%\w*)*(\?(\w*[-&=\w]*)*)*([#]\w*)?";
+            var match = Regex.Match(urlStr, urlRegex, RegexOptions.IgnorePatternWhitespace);
+            if (match.Success)
+            {
+                this.Scheme = match.Groups[1].ToString();
+                this.Host = match.Groups[3].ToString();
+                if (match.Groups[4].ToString() != "")
+                {
+                    this.Port = int.Parse(match.Groups[4].ToString().Replace(":", ""));
+                }
+                this.Path = Decode(match.Groups[5].ToString());
+                this.Query = match.Groups[6].ToString().Replace("?", "");
+                this.FragmentId = match.Groups[8].ToString().Replace("#", "");
+            }
+            else
+            {
+                throw new FormatException("Could not parse URL: " + urlStr);
+            }
+
+        }
+
+        /// <summary>
+        /// Constructor for building URLs from their components.
+        /// </summary>
+        public Url(string scheme, string host, int port, string path, string query, string fragmentId)
+        {
+            this.Scheme = scheme;
+            this.Host = host;
+            this.Port = port;
+            this.Path = path;
+            this.Query = query;
+            this.FragmentId = fragmentId;
+        }
+
+        /// <summary>
+        /// Returns the string representation of the URL.
+        /// </summary>
+        public override string ToString()
+        {
+            string url = Scheme+"://"+Host+":"+Port+Encode(Path)+"?"+Query+"#"+FragmentId;
+
+            return url;
+        }
+
+        /// <summary>
+        /// Encodes any special characters in the URL with an escaping sequence.
+        /// </summary>
+        public static string Encode(string s)
+        {
+            string result = s.Replace(" ", "%20");
+
+            return result;
+        }
+
+        /// <summary>
+        /// Decodes any escaping sequence in the URL with the corresponding characters.
+        /// </summary>
+        public static string Decode(string s)
+        {
+            return s.Replace("%20"," ");
+        }
+    }
+}
---
+++ Tutorial-03/Task3-Template/HttpMessage.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Net.WebRequestMethods;
+
+namespace SSE
+{
+    public class HttpMessage
+    {
+        public const string GET = "GET";
+        public const string POST = "POST";
+        public const string PUT = "PUT";
+        public const string DELETE = "DELETE";
+        public const string HEAD = "HEAD";
+        public const string OPTIONS = "OPTIONS";
+        public const string TRACE = "TRACE";
+
+        public string Method = "";
+        public string Host = "";
+        public string Resource = "";
+        public Dictionary<string, string> Headers = new Dictionary<string, string>();
+        public string Content = "";
+        public string StatusCode = "";
+        public string StatusMessage = "";
+
+        /// <summary>
+        /// Construct an HTTP request.
+        /// </summary>
+        public HttpMessage(string method, string host, string resource, Dictionary<string, string> headers, string content)
+        {
+            this.Method = method;
+            this.Host = host;
+            this.Resource = resource;
+            this.Headers = headers;
+            if (Headers == null)
+                Headers = new Dictionary<string, string>();
+            this.Content = content;
+            StatusCode = null;
+            StatusMessage = null;
+        }
+
+        /// <summary>
+        /// Construct an HTTP response.
+        /// </summary>        
+        public HttpMessage(string statusCode, string statusMessage, Dictionary<string, string> headers, string content)
+        {
+            this.Method = null;
+            this.Host = null;
+            this.Resource = null;
+            this.Headers = headers;
+            if (Headers == null)
+                Headers = new Dictionary<string, string>();
+            this.Content = content;
+            StatusCode = statusCode;
+            StatusMessage = statusMessage;
+        }
+
+        /// <summary>
+        /// Constructs an HTTP message by parsing a (received) string.
+        /// </summary>
+        /// <param name="message"></param>
+        public HttpMessage(string message)
+        {
+            //Response
+            string methodRegex = @"(GET|HEAD|POST|PUT|DELETE|OPTIONS|TRACE)";
+            this.Method = matchRequest(methodRegex, message, 1);
+
+            string contentTypeRegex = @"(Content-Type: )([\w*\/]*)";
+            this.Headers.Add("Content-Type", matchRequest(contentTypeRegex, message, 2));
+
+            string contentLengthRegex = @"(Content-Length: )(\d*)";
+            this.Headers.Add("Content-Length", matchRequest(contentLengthRegex, message, 2));
+            
+            string contentRegex = @"(\n)?(([\w( )])*(\n)?)$";
+            this.Content = matchRequest(contentRegex, message, 2);
+
+            string statusCodeRegex = @"(HTTP\/\d.\d )(\d*)";
+            this.StatusCode = matchRequest(statusCodeRegex, message, 2);
+
+            string statusMessageRegex = @"(HTTP\/\d.\d \d* )(\w*)";
+            this.StatusMessage = matchRequest(statusMessageRegex, message, 2);
+
+
+            //Request
+            string resourceRegex = @"(GET|HEAD|POST|PUT|DELETE|OPTIONS|TRACE) ([\w*\/]*)";
+            this.Resource = matchRequest(resourceRegex, message, 2);
+
+            string hostRegex = @"(Host: )([\w*\/.]*)(\n)";
+            this.Host = matchRequest(hostRegex, message, 2);
+
+        }
+
+        private string matchRequest(string regexStr, string requestMsg, int valuePosition)
+        {
+            var match = Regex.Match(requestMsg, regexStr, RegexOptions.None);
+            if (match.Success)
+            {
+                return match.Groups[valuePosition].ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Returns the string representation of the message.
+        /// </summary>        
+        public override string ToString()
+        {
+            // TODO: construct HTTP request/response message
+            string url = "";
+            // Request
+            if (this.Method != ""){
+                url += this.Method;
+                url += " " + this.Resource + " HTTP/1.1";
+                url += "\nHost: " + this.Host;
+            }
+            // Response
+            if (this.Method == "")
+            {
+                url += "HTTP/1.1 " + this.StatusCode;
+                url += " " + this.StatusMessage;
+                url += "\nContent-Type: " + this.Headers["Content-Type"];
+            }
+            url += "\nContent-Length: " + this.Headers["Content-Length"] + "\n";
+            url += "\n" + this.Content;
+            return url;
+        }
+    }
+}
